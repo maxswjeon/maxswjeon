0e81903d207a8dfa3166785b84fdb2cbfa1d6e05
Jeon Sangwan <maxswjeon@naver.com>
2022-01-02 14:42:22 +0900

    Add Certificate Section

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import Character from './images/icons/character.png';
 import ContactSection from './sections/ContactSection';
 import EducationSection from './sections/EducationSection';
 import ProjectSection from './sections/ProjectSection';
+import CertificateSection from './sections/CertificateSection';
 
 function App() {
     return (
-        <div className="print:w-[21cm] print:h-[29.7cm] print:border-none print:m-0 print:p-0 container mx-auto sm:my-12 my-0 p-6 sm:shadow-card rounded-3xl">
-            <header className="flex mb-3 border-b-2 border-b-gray-200 py-2">
-                <div className="flex grow align-bottom items-end">
+        <div className="print:w-[21cm] print:h-[29.7cm] print:border-none print:m-0 print:p-0 container mx-auto sm:my-12 p-6 sm:shadow-card rounded-3xl">
+            <header className="flex mb-3 py-2">
+                <div className="flex grow align-bottom items-end  border-b-8 border-b-[#c4865f]">
                     <h1 className="print:text-[24pt] text-4xl font-bold mb-3 align-text-bottom">
                         전상완 <br className="sm:hidden block" />
                         <span className="print:text-[16pt] text-2xl font-bold text-gray-500">Sangwan Jeon</span>
                     </h1>
                 </div>
                 <img
-                    className="rounded-full print:w-[3.5cm] print:h-[3.5cm] box-border print:m-[0.5cm] w-[27.5vw] h-[27.5vw] max-w-[125px] max-h-[125px]"
+                    className="rounded-full print:w-[3.5cm] print:h-[3.5cm] box-border print:m-[0.5cm] w-[27.5vw] h-[27.5vw] max-w-[125px] max-h-[125px] ml-6"
                     src={Character}
                     alt="Profile Image"
                 />
@@ -22,6 +23,7 @@ function App() {
             <ContactSection />
             <EducationSection />
             <ProjectSection />
+            <CertificateSection/>
         </div>
     );
 }
---
+++ src/sections/CertificateSection.tsx
@@ -0,0 +1,15 @@
+const CertificateSection = () => (
+    <section className="mb-3 border-b-2 border-b-gray-200 py-2">
+        <h2 className="print:text-[16pt] font-bold text-2xl">Certificates</h2>
+        <div className="grid grid-cols-12 print:text-[11pt] text-lg gap-3">
+            <p className="md:col-span-2 col-span-3 text-gray-500">2020.12.</p>
+            <p className="md:col-span-10 col-span-9">TOEIC (940/990)</p>
+            <p className="md:col-span-2 col-span-3 text-gray-500">2021.10.</p>
+            <p className="md:col-span-10 col-span-9">아마추어무선통신기사(전화) 3급</p>
+            <p className="md:col-span-2 col-span-3 text-gray-500">2021.12.</p>
+            <p className="md:col-span-10 col-span-9">정보처리기능사</p>
+        </div>
+    </section>
+);
+
+export default CertificateSection;
