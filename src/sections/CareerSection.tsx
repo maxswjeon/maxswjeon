53e34361c34e5d4833bbb86859ac4c4347dd949e
Jeon Sangwan <maxswjeon@naver.com>
2022-03-05 18:25:00 +0900

    Fix CareerSection to Korean

--- src/sections/CareerSection.tsx
+++ src/sections/CareerSection.tsx
@@ -4,8 +4,7 @@ const CareerSection = () => {
             <h2 className="print:text-[16pt] text-2xl font-bold mb-2">Career</h2>
             <div className="grid grid-cols-12 print:text-[11pt] text-lg gap-3">
                 <p className="md:col-span-2 col-span-3 text-gray-500">2022.01 ~ 2022.02</p>
-                <p className="md:col-span-10 col-span-9"><a href="https://promedius.ai">Promedius Inc.</a>, as
-                    Infrastructure Team Member</p>
+                <p className="md:col-span-10 col-span-9"><a href="https://promedius.ai">프로메디우스</a>에서 인프라팀 인턴으로</p>
             </div>
         </section>
     )
